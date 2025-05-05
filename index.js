41d0d441be0c99e95adcea88ccb7a16dcfb79e94
Sriragbadveti <badvetisrirag@gmail.com>
2025-05-05 08:40:09 +0400

    finally

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dashboard-app-frontend-eight.vercel.app",
     credentials: true,
   })
 );
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -40,7 +40,7 @@ router.post("/register", async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       })
@@ -88,7 +88,7 @@ router.post("/login", async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       })
