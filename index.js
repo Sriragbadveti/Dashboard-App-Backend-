6936d9bd79d0bec7123747a99bcfbfc144050f77
Sriragbadveti <badvetisrirag@gmail.com>
2025-05-05 08:34:07 +0400

    try

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://dashboard-app-frontend-eight.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
